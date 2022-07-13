e827fe07b1c0d3e19fbab46501e35530cf3d25fb
johann bich <johannbich@gmail.com>
2022-07-13 13:01:19 +0200

    feat: scripts to concatenate json file into 1

---
+++ scripts/concatenate.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+current=$(pwd)
+jsonFolder="${current}/batch-upload/raw"
+jsonArray=$(find ${jsonFolder} -type f -name '*.json')
+outputfile="$current/output.json" 
+echo "[" > $outputfile
+for file in $jsonArray
+do
+  cat $file >> $outputfile
+  echo "," >> $outputfile
+done
+echo "]" >> $outputfile
